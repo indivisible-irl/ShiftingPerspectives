82678eb46ea8534bbcc152211a35e2f0fe9a7e70
indivisible-irl <github@indivisibleindustries.com>
2013-12-21 06:30:33 +0000

    last commit, about to delete

--- src/com/indivisible/shiftingperspectives/scheduler/TickMonitorScheduler.java
+++ src/com/indivisible/shiftingperspectives/scheduler/TickMonitorScheduler.java
@@ -2,24 +2,57 @@ package com.indivisible.shiftingperspectives.scheduler;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import com.indivisible.shiftingperspectives.actions.Announcer;
+import com.indivisible.shiftingperspectives.actions.Announce;
 
 
 public class TickMonitorScheduler
 {
 
+    //// data
+
     private JavaPlugin plugin;
-    private Announcer announcer;
+    private Announce announcer;
+    private int nextTaskID = Integer.MIN_VALUE;
 
     private static final long UPDATE_FREQ = 600L;
 
-    public TickMonitorScheduler(JavaPlugin jPlugin, Announcer anouncer)
+
+    //// constructors & init
+
+    public TickMonitorScheduler(JavaPlugin jPlugin, Announce announcer)
     {
         this.plugin = jPlugin;
+
+        //REM add new timed actions as and when written
         this.announcer = announcer;
-        //TODO add new timed actions as and when written
     }
 
+
+    //// public methods
+
+    public void startUpdating()
+    {
+        //ASK run everything once then queue as normal or just queue from now?
+
+    }
+
+    public void stopUpdating()
+    {
+        if (nextTaskID != Integer.MIN_VALUE)
+        {
+            plugin.getServer().getScheduler().cancelTask(nextTaskID);
+        }
+    }
+
+
+    //// private methods
+
+    private void performUpdate()
+    {
+        //TODO loop through actions and trigger their triggers
+        long now = plugin.getServer().getWorld("world").getFullTime();
+
+    }
 
     private void queueUpdate()
     {
@@ -28,25 +61,16 @@ public class TickMonitorScheduler
     }
 
 
+    //// runnable
+
     private class Update
             extends BukkitRunnable
     {
 
-        private boolean announce;
-        private boolean shift;
-
-        //        public Update(boolean runAnnouncer)
-        //        {
-        //            announce = runAnnouncer;
-        //        }
-
         @Override
         public void run()
         {
-            //            if (announce)
-            //            {
-            //                announcer.
-            //            }
+            performUpdate();
         }
 
     }
