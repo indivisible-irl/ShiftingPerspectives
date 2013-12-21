6472617254d94e413e470c7368d9c7fcb5a7871c
indivisible-irl <github@indivisibleindustries.com>
2013-12-21 06:31:46 +0000

    moved TickMonitor functionality to Main class

--- src/com/indivisible/shiftingperspectives/ShiftingPerspectives.java
+++ src/com/indivisible/shiftingperspectives/ShiftingPerspectives.java
@@ -3,34 +3,95 @@ package com.indivisible.shiftingperspectives;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import com.indivisible.shiftingperspectives.actions.Announce;
 
-public final class ShiftingPerspectives extends JavaPlugin
+public final class ShiftingPerspectives
+        extends JavaPlugin
 {
 
-	@Override
-	public void onEnable()
-	{
-		//TODO verify WorldGuard is installed
-		
-	}
-	
-	@Override
-	public void onDisable()
-	{
-		
-	}
+    //// data
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args)
-	{
-		// command root to be '/shift <args>'
-		
-		//ASK false if not catching command?
-		return false;
-	}
+    private Announce announcer;
+    private int nextTaskID = Integer.MIN_VALUE;
+    private static final long UPDATE_FREQ = 600L;
 
-	
 
-	
+    //// plugin methods
+
+    @Override
+    public void onEnable()
+    {
+        this.announcer = new Announce(this);
+
+        startUpdating();
+    }
+
+    @Override
+    public void onDisable()
+    {
+        stopUpdating();
+        this.announcer = null;
+    }
+
+
+    //// enable and disable the scheduling
+
+    private void startUpdating()
+    {
+        //ASK run everything once then queue as normal or just queue from now?
+
+    }
+
+    private void stopUpdating()
+    {
+        if (nextTaskID != Integer.MIN_VALUE)
+        {
+            this.getServer().getScheduler().cancelTask(nextTaskID);
+        }
+    }
+
+    private void performUpdate()
+    {
+        //TODO loop through actions and trigger their triggers
+        long now = this.getServer().getWorld("world").getFullTime();
+
+    }
+
+    private void queueUpdate()
+    {
+        Update update = new Update();
+        this.getServer().getScheduler().runTaskLater(this, update, UPDATE_FREQ);
+    }
+
+
+    //// runnable
+
+    private class Update
+            extends BukkitRunnable
+    {
+
+        @Override
+        public void run()
+        {
+            performUpdate();
+        }
+
+    }
+
+    //// command handling
+
+    @Override
+    public boolean onCommand(CommandSender sender,
+                             Command command,
+                             String label,
+                             String[] args)
+    {
+        // command root to be '/shift <args>'
+
+        //ASK false if not catching command?
+        return false;
+    }
+
+
 }
