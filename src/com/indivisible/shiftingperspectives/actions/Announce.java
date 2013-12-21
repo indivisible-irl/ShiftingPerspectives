ffec04d9eaef8e4b23537452d96202dc70075aae
indivisible-irl <github@indivisibleindustries.com>
2013-12-21 04:42:34 +0000

    reverted name back, silly me. need coffee

--- src/com/indivisible/shiftingperspectives/actions/Announce.java
+++ src/com/indivisible/shiftingperspectives/actions/Announce.java
@@ -4,7 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 
-public class Announcer
+public class Announce
 {
 
     //// data
@@ -20,7 +20,7 @@ public class Announcer
 
     //// constructors & init
 
-    public Announcer(JavaPlugin jPlugin)
+    public Announce(JavaPlugin jPlugin)
     {
         doAnnounce = jPlugin.getConfig().getBoolean("announce.display", false);
         if (doAnnounce)
